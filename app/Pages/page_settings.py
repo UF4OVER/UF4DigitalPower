09ca88fbc4ea56365583c74af17cc0fc970edb41
UF4 <heping@hepi.ng>
2026-04-29 23:47:04 +0800

    feat: 测试提交

--- app/Pages/page_settings.py
+++ app/Pages/page_settings.py
@@ -87,7 +87,7 @@ class SettingsPage(ScrollArea):
 
     def __initWidget(self):
         self.resize(1000, 800)
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setViewportMargins(0, 80, 0, 20)
         self.setWidget(self.scrollWidget)
         self.setWidgetResizable(True)
